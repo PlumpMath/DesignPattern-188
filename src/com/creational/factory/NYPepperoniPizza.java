1bc0cc3c90010a383841b52f8146c47423fdb42a
Jianqing Guan <gjqvictory888@gmail.com>
2016-06-28 21:47:46 -0400

    factory design pattern

---
+++ src/com/creational/factory/NYPepperoniPizza.java
@@ -0,0 +1,9 @@
+package com.creational.factory;
+
+public class NYPepperoniPizza extends Pizza {
+	String name = "New York Pepperoni Pizza";
+	String dough = "Thin crust dough";
+	String sauce = "Marinara sauce";
+	String toppings = "Grated Reggiano Cheese";
+	
+}
